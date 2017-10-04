493a5407594ee747f202e8874bfaa39ac0db2604
DavidTsyg <davidtsyg@gmail.com>
2017-10-04 08:23:30 +0300

    Add files via upload

---
+++ OptimizationMethodsLab1.cpp
@@ -0,0 +1,21 @@
+// OptimizationMethodsLab1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "Functions.h"
+
+int main()
+{
+	Simplex_Table table;
+	table.Create_Simplex_Table();
+	table.Print_Problem();
+	table.Print_Canonized_Problem();
+	table.Print_Simplex_Table();
+	table.Basis_Solution();
+	table.Simplex_Method();
+	table.Just_In_Case();
+	system("pause");
+	return 0;
+
+}
+
