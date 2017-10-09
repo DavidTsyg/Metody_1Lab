de1ef90bd5a4c5f6e35a7ed1530b4c1454e2f0f5
DavidTsyg <davidtsyg@gmail.com>
2017-10-09 10:47:27 +0300

    Update OptimizationMethodsLab1.cpp

--- OptimizationMethodsLab1.cpp
+++ OptimizationMethodsLab1.cpp
@@ -11,9 +11,14 @@ int main()
 	table.Print_Problem();
 	table.Print_Canonized_Problem();
 	table.Print_Simplex_Table();
-	table.Basis_Solution();
+//	table.Basis_Solution();
+	table.Opornoe_Solution();
 	table.Simplex_Method();
-	table.Just_In_Case();
+//	table.Just_In_Case();
+	table.Transform_Into_Twin();
+	table.Opornoe_Solution();
+	table.Simplex_Method();
+//	table.Just_In_Case();
 	system("pause");
 	return 0;
 
